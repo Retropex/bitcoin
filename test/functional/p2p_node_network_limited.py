00f9900203717e2f93007d3ebb99abd1130ca966
Peter Todd <pete@petertodd.org>
2024-09-08 16:05:00 +0000

    Peer with 4 other Libre Relay nodes
    Also, set FULL_RBF nService bit if full-rbf enabled.

--- test/functional/p2p_node_network_limited.py
+++ test/functional/p2p_node_network_limited.py
@@ -14,6 +14,8 @@ from test_framework.messages import (
     NODE_NETWORK_LIMITED,
     NODE_P2P_V2,
     NODE_WITNESS,
+    NODE_REPLACE_BY_FEE,
+    NODE_PREFERENTIAL_PEERING,
     msg_getdata,
 )
 from test_framework.p2p import P2PInterface
@@ -122,7 +124,7 @@ class NodeNetworkLimitedTest(BitcoinTestFramework):
     def run_test(self):
         node = self.nodes[0].add_p2p_connection(P2PIgnoreInv())
 
-        expected_services = NODE_WITNESS | NODE_NETWORK_LIMITED
+        expected_services = NODE_WITNESS | NODE_NETWORK_LIMITED | NODE_REPLACE_BY_FEE
         if self.options.v2transport:
             expected_services |= NODE_P2P_V2
 
